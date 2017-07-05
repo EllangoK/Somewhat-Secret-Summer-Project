cf6c6fef1664e497916318d3259e2fd060a8ba79
Karun Ellango <ellangok@bxscience.edu>
2017-07-05 01:34:31 -0400

    Added the other motor
    We were missing the Y motor,  so that was added and its related stuff was added

--- Transducer Controller/manualAxisControl.py
+++ Transducer Controller/manualAxisControl.py
@@ -4,7 +4,7 @@ from tkinter import *
 #from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
 import time
 import atexit
-from Variables import stepsPerRotation, xPins, stepDegrees, microsteps
+from Variables import stepsPerRotation, xPins, stepDegrees, microsteps, yPins
 
 #mh = Adafruit_MotorHAT()
 
@@ -24,7 +24,8 @@ microstepsBack = 0
 
 #atexit.register(turnOffMotors)
 
-#myStepper = mh.getStepper(stepsPerRotation, xPins)
+#myStepperX = mh.getStepper(stepsPerRotation, xPins)
+#myStepperY = mh.getStepper(stepsPerRotation, yPins)
 
 languages = [
 ("1/8 Microstep / 0.225 Degrees", 1),
@@ -40,44 +41,44 @@ if choice == "manualX":
     def one_eight():
         global degreesMoved
         degreesMoved = degreesMoved + 0.225
-        #myStepper.step(((1/8)*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperX.step(((1/8)*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("One Eight")
  
     def oneStep():
         global degreesMoved 
         degreesMoved = degreesMoved + 1.8
-        #myStepper.step((1*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperX.step((1*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("One step")
  
     def fortyFiveDegrees():
         global degreesMoved 
         degreesMoved = degreesMoved + 45
-        #myStepper.step((25*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperX.step((25*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("45")
 
     def ninetyDegrees():
         global degreesMoved
         degreesMoved = degreesMoved + 90
-        #myStepper.step((50*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperX.step((50*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("90") 
 
     def hundredEightyDegrees():
         global degreesMoved 
         degreesMoved = degreesMoved + 180
-        #myStepper.step((100*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperX.step((100*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("180")
 
     def threeHundredSixtyDegrees():
         global degreesMoved 
         degreesMoved = degreesMoved + 360
-        #myStepper.step((200*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperX.step((200*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("360")
 
     def reset():    
         global degreesMoved 
         global microstepsBack
         microstepsBack = (microsteps*(degreesMoved/stepDegrees))
-        #myStepper.step(microstepsBack, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperX.step(microstepsBack, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("Reset")
 
     options = {1 : one_eight,
@@ -115,44 +116,44 @@ elif choice == "manualY":
     def one_eight():
         global degreesMoved
         degreesMoved = degreesMoved + 0.225
-        #myStepper.step(((1/8)*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperY.step(((1/8)*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("One Eight")
  
     def oneStep():
         global degreesMoved 
         degreesMoved = degreesMoved + 1.8
-        #myStepper.step((1*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperY.step((1*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("One step")
  
     def fortyFiveDegrees():
         global degreesMoved 
         degreesMoved = degreesMoved + 45
-        #myStepper.step((25*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperY.step((25*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("45")
 
     def ninetyDegrees():
         global degreesMoved
         degreesMoved = degreesMoved + 90
-        #myStepper.step((50*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperY.step((50*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("90") 
 
     def hundredEightyDegrees():
         global degreesMoved 
         degreesMoved = degreesMoved + 180
-        #myStepper.step((100*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperY.step((100*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("180")
 
     def threeHundredSixtyDegrees():
         global degreesMoved 
         degreesMoved = degreesMoved + 360
-        #myStepper.step((200*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperY.step((200*8), Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("360")
 
     def reset():    
         global degreesMoved 
         global microstepsBack
         microstepsBack = (microsteps*(degreesMoved/stepDegrees))
-        #myStepper.step(microstepsBack, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
+        #myStepperY.step(microstepsBack, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
         print ("Reset")
 
     options = {1 : one_eight,
