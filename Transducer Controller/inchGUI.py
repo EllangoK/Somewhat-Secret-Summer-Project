48809c4275ec28a817a9dd5aa79c20b9d3602e70
EllangoK <ellangok@bxscience.edu>
2017-07-22 22:29:03 -0400

    Inch interface
    Not even done yet just the backbone

---
+++ Transducer Controller/inchGUI.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+inchMovements = [
+("0.25 inches", 1),
+("0.5 inches", 2),
+("1 inch", 3),
+("2 inches", 4),
+("Custom Value", 5)
+]
+
+def
+
+options = {1 : point25,
+           2 : point5,
+           3 : oneInch,
+           4 : twoInch,
+           5 : customVal 
+}
