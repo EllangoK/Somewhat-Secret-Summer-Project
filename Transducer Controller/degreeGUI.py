0ac8721743205a2120bf90412a9dd04c48645aed
EllangoK <ellangok@bxscience.edu>
2017-07-22 22:32:15 -0400

    Degree Interface
    Not even done yet just the backbone

---
+++ Transducer Controller/degreeGUI.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+degreeMovements = [
+("1.8 inches", 1),
+("45 inches", 2),
+("90 inch", 3),
+("180 inches", 4),
+("360 degrees", 5),
+("Custom Value", 6)
+]
+
+options = {1 : onePointEight,
+           2 : fortyFive,
+           3 : ninety,
+           4 : oneEighty,
+           5 : threeSixty,
+           6 : customVal
+}
