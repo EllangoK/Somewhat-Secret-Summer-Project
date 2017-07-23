2ffd0960ac573d8aa012c7d60da404636c718460
EllangoK <ellangok@bxscience.edu>
2017-07-22 22:35:44 -0400

    MM Interface
    If I just waited i could have put them in the same commit.

---
+++ Transducer Controller/mmGUI.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+mmMovements = [
+("1 mm", 1),
+("2 mm", 2),
+("5 mm", 3),
+("10 mm", 4),
+("25 mm", 5)
+("50 mm", 6),
+("100 mm", 7),
+("250 mm", 8),
+("Custom Value", 9),
+]
+
+options = {1 : oneMM,
+           2 : twoMM,
+           3 : fiveMM,
+           4 : tenMM,
+           5 : twentyFiveMM,
+           6 : fiftyMM,
+           7 : hundredMM,
+           8 : twoFiftyMM,
+           9 : customVal,
+}
